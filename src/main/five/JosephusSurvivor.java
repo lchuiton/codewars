1bc43d57071e854accb7fefbceb19fe9a97f67f9
lchuiton <lchuiton@lchuiton.com>
2018-05-26 18:55:04 -0400

    JosephusSurvivor Complete

--- src/main/five/JosephusSurvivor.java
+++ src/main/five/JosephusSurvivor.java
@@ -1,15 +1,25 @@
 package five;
 
-public class JosephusSurvivor {
-    public static int josephusSurvivor(final int n, final int k) {
-        int[] array = new int[n];
-        for (int i = 0; i<k; i++) {
-         //   array[i] =
+import java.util.ArrayList;
+import java.util.List;
 
+public class JosephusSurvivor {
+    public static int josephusSurvivor(int n, int k) {
+
+        List<Integer> survivors = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            survivors.add(i + 1);
         }
 
+        int positionToRemove = 0;
+        while (survivors.size() > 1) {
+            positionToRemove = (positionToRemove + k - 1) % survivors.size();
+            survivors.remove(positionToRemove);
+        }
 
-        return array[0];
+        return survivors.get(0);
 
     }
+
 }
--- src/test/five/JosephusSurvivorTest.java
+++ src/test/five/JosephusSurvivorTest.java
@@ -1,37 +1,31 @@
 package five;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class JosephusSurvivorTest {
 
-    @Ignore
     @Test
     public void test1() {
         josephusTest(7, 3, 4);
     }
 
-    @Ignore
     @Test
     public void test2() {
         josephusTest(11, 19, 10);
     }
 
-    @Ignore
     @Test
     public void test3() {
         josephusTest(40, 3, 28);
     }
 
-    @Ignore
     @Test
     public void test4() {
         josephusTest(14, 2, 13);
     }
 
-    @Ignore
     @Test
     public void test5() {
         josephusTest(100, 1, 100);
